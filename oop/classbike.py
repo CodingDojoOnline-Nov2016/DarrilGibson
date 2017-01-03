52a3e34ae1ea1123e483dc0f59c19df238cd7208
Darril <darril@cox.net>
2017-01-03 12:35:33 -0500

    Add files via upload

---
+++ oop/classbike.py
@@ -0,0 +1,38 @@
+class Bike(object):
+    # def __init__(self):
+    def __init__(self, price, max_speed):
+      print "New bike created"
+      self.price = price
+      self.max_speed = max_speed
+      self.miles = 0
+
+    def displayinfo(self):
+        print "Price is $" + str(self.price)
+        print "Max speed is " + str(self.max_speed) + ' MPH'
+        print "Current mileage is " + str(self.miles) + ' miles'
+
+    def ride(self):
+        print "Riding"
+        self.miles = self.miles + 10
+        print "Current mileage is " + str(self.miles) + ' miles'
+
+
+    def reverse(self):
+        print "Reversing"
+        self.miles = self.miles - 5
+        print "Current mileage is " + str(self.miles) + ' miles'
+
+
+Racer = Bike(119.99, 25)
+Racer.ride()
+Racer.ride()
+Racer.ride()
+Racer.reverse()
+Racer.displayinfo()
+
+mountain = Bike(399.99, 20)
+mountain.ride()
+mountain.ride()
+mountain.reverse()
+mountain.reverse()
+mountain.displayinfo()

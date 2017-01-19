ecfebece2a41b4a7b35bc985b2b6b87a5d5e05ec
Darril <darril@cox.net>
2017-01-19 10:01:07 -0500

    Add files via upload

---
+++ django/Wall_ERD_models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Message(models.Model):
+    message = models.CharField(max_length=1000)
+    user_id = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Comment(models.Model):
+    title = models.CharField(max_length=45)
+    comment = models.CharField(max_length=1000)
+    user_id = models.ForeignKey(User)
+    message_id = models.ForeignKey(Message)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
